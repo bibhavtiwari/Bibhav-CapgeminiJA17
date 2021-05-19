e085276fb18d7794c9fe29ececeab943b71619cf
bibhavtiwari <66523672+bibhavtiwari@users.noreply.github.com>
2021-05-19 16:53:11 +0530

    Add files via upload

---
+++ labbook2/SecondSmallestElement.java
@@ -0,0 +1,41 @@
+package labbook2;
+import java.util.Scanner;
+public class SecondSmallestElement {
+	static int getSecondSmallest(int arr[],int n)
+	 {
+		 int i,j,t;
+		 n=arr.length;
+		 for(i=0;i<n;i++) {
+			 for(j=0;j<n;j++)
+			 {
+				 if(arr[i]<arr[j])
+				 {
+					 t=arr[i];
+					 arr[i]=arr[j];
+					 arr[j]=t;
+				 }
+			 }
+		 }
+		 return arr[1];
+	 }
+	 
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int i;
+			System.out.println("Enter the no. of elements");	
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			System.out.println("enter the elements of array:-");
+			for(i=0;i<n;i++)
+			{
+				a[i] = sc.nextInt();
+			}
+			SecondSmallestElement  num= new SecondSmallestElement();
+			
+			 num.getSecondSmallest(a,n);
+
+			System.out.println("Second Smallest element: "+a[1]);
+			
+			
+		}
+}
